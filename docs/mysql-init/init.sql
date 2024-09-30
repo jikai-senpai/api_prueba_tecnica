0418c2209e21ff5fd7611722533fed89cf0a4fea
jikai-senpai <thejasondutys@hotmail.com>
2024-09-30 00:35:39 -0400

    probando lo de docker

--- docs/mysql-init/init.sql
+++ docs/mysql-init/init.sql
@@ -1,10 +1,9 @@
-CREATE DATABASE PRUEBA_TECNICA;
+CREATE DATABASE IF NOT EXISTS PRUEBA_TECNICA;
 
 USE PRUEBA_TECNICA;
 
 -- Tabla contactos
-CREATE TABLE
-    `contactos`
+CREATE TABLE IF NOT EXISTS `contactos`
 (
     `id`                 int          NOT NULL AUTO_INCREMENT,
     `nombre`             varchar(255) NOT NULL,
@@ -12,14 +11,13 @@ CREATE TABLE
     `email`              varchar(255) NOT NULL,
     `estado`             boolean DEFAULT TRUE     NOT NULL,
     PRIMARY KEY (`id`)
-);
+    );
 
 -- Tabla telefonos
-CREATE TABLE
-    `telefonos`
+CREATE TABLE IF NOT EXISTS `telefonos`
 (
     `id`            int         NOT NULL AUTO_INCREMENT,
     `numero`        varchar(255) NOT NULL,
     `id_contacto`   int         NOT NULL,
     PRIMARY KEY (`id`)
-);
+    );
